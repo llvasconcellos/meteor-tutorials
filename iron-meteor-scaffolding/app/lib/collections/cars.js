96f36af8399a34b20c92782153c5b6e9ad09fe31
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2016-09-16 01:48:36 -0300

    created cars collection

---
+++ iron-meteor-scaffolding/app/lib/collections/cars.js
@@ -0,0 +1,32 @@
+Cars = new Mongo.Collection('cars');
+
+
+if (Meteor.isServer) {
+  Cars.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Cars.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
